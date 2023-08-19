72e1360bfc99d63addbf9c2af6ed5ba5869436cd
nobleaustine <nobleaustine2001@gmail.com>
2023-08-19 20:44:45 +0530

    new home page

--- Server/source/index.js
+++ Server/source/index.js
@@ -4,6 +4,7 @@ const bodyParser =require('body-parser')
 const connectDB = require('./database.js');
 
 const tureONianRoute = require('./routes/tureONian.js')
+const path = require("path")
 
 connectDB()
 const app = express()
@@ -18,6 +19,18 @@ app.use(tureONianRoute);
 app.use((req,res,next)=>{
     res.status(404).send('you are lost my friend......')
 })
+
+// ------------ Deployment ---------------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+
+   app.use(express.static(path.join(__dirname1,"/Client/build")));
+   app.get('*',(req,res)=>{ res.sendFile(path.resolve(__dirname1,"Client","build","index.html")) })
+}else{
+    app.get('/',(req,res)=>{ res.send("API is running ") });
+
+}
+// ------------ Deployment ---------------
 
 const PORT = process.env.PORT 
 app.listen(PORT,()=> console.info(`Server has started on port ${PORT}......`)) 
