8376c06000f9e35fa93bc373a8588cd929857a60
nobleaustine <nobleaustine2001@gmail.com>
2023-08-19 23:53:47 +0530

    package hosting issue resolved

---
+++ Server/Client/build/precache-manifest.ac41d847599a4d1c24fea2fc1d27d422.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c162dc924242a05c5a",
+    "url": "/static/css/main.e390d05b.chunk.css"
+  },
+  {
+    "revision": "42c162dc924242a05c5a",
+    "url": "/static/js/main.9f50aae5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a15fcc5dbde2849f99a",
+    "url": "/static/js/2.e822a8aa.chunk.js"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/static/media/video.ed44dc5d.mp4"
+  },
+  {
+    "revision": "2b24408aa65851e0775214622e7d73f8",
+    "url": "/static/media/turingMachine.2b24408a.svg"
+  },
+  {
+    "revision": "a5c7e5ba91f01eec82416bbf3f55da0f",
+    "url": "/static/media/img(1).a5c7e5ba.svg"
+  },
+  {
+    "revision": "b0094ff2cddc31bb9d8a63ec3eba57a8",
+    "url": "/static/media/img.b0094ff2.svg"
+  },
+  {
+    "revision": "e7ec751a49e0379aef25d12f7f2437ca",
+    "url": "/static/media/img(2).e7ec751a.svg"
+  },
+  {
+    "revision": "8f1ddb6fdb27986e30da64a68be2b079",
+    "url": "/index.html"
+  }
+];
